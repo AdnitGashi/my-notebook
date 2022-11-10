27f694b419c13aafeb65104443e89d23f29d9ae2
AdnitGashi <66799426+AdnitGashi@users.noreply.github.com>
2022-11-10 23:34:01 +0100

    Added tailwind

---
+++ src/Components/Sidebar/NoteType/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const NoteType = () => {
+  return <div className="bg-red-200">Notetype</div>;
+};
+export default NoteType;
---
+++ src/Components/Sidebar/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NoteType from "./NoteType";
+
+const Sidebar = () => {
+  return (
+    <div className="">
+      <NoteType />
+    </div>
+  );
+};
+export default Sidebar;
